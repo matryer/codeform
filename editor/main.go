2b74e35c00afe667b1cfc7058a6b4927fda599ad
Mat Ryer <thatmatryer@gmail.com>
2017-01-09 12:42:25 +0000

    added note on running editor

--- editor/main.go
+++ editor/main.go
@@ -1,3 +1,6 @@
+// Package editor provides the editor web tool.
+// To run with Google App Engine, install the Google App Engine Go SDK
+// and go `goapp serve` in this directory.
 package editor
 
 import (
@@ -8,10 +11,15 @@ import (
 )
 
 func init() {
+	http.Handle("/", New())
+}
+
+// New makes a new http.Handler that provides the editor.
+func New() http.Handler {
 	router := NewRouter()
 	router.contexter = ContexterFunc(func(r *http.Request) context.Context {
 		return appengine.NewContext(r)
 	})
 	routes(router)
-	http.Handle("/", router)
+	return router
 }
