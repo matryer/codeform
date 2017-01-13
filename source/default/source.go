60f6294b141316dbdbc64e0d6b5aaf97c8db6489
Mat Ryer <thatmatryer@gmail.com>
2017-01-13 17:14:18 +0000

    added comments

--- source/default/source.go
+++ source/default/source.go
@@ -5,41 +5,50 @@
 // features.
 package defaultsource
 
+// Interface1 is an interface.
 type Interface1 interface {
 	Method1()
 }
 
+// Interface2 is an interface.
 type Interface2 interface {
 	Method2A(int)
 	Method2B(a, b int)
 }
 
+// Interface3 is an interface.
 type Interface3 interface {
 	Method3A() error
 	Method3B(int) error
 	Method3C(a, b int) (bool, error)
 }
 
+// Struct1 is a struct.
 type Struct1 struct {
 	Field1 int
 }
 
+// Struct2 is a struct.
 type Struct2 struct {
 	Field1 int
 	Field2 bool
 	Field3 string
 }
 
+// Struct3 is a struct.
 type Struct3 struct {
 	Field1 string
 }
 
+// Func1 is a function.
 func Func1() {}
 
+// Func2 is a function.
 func Func2(a, b int) int {
 	return a
 }
 
+// Func3 is a function.
 func Func3(a, b int) (bool, error) {
 	return true, nil
 }
